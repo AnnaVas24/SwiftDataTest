9f25fad10808bb23678dbd81f85e27a3536bc8b0
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-06-10 14:26:21 +0300

    Initial Commit

---
+++ SwiftDataTest/SwiftDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTestApp.swift
+//  SwiftDataTest
+//
+//  Created by Vasichko Anna on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
